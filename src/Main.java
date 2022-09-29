b99c2d7fe69de330cfd8bc280f2a25f2d4e5a2b3
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:14:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
+/*
+Dairenin Alanını ve Çevresini Hesaplayan Program
+Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan programı yazın.
+
+Alan Formülü : π * r * r;
+
+Çevre Formülü : 2 * π * r;
+
+Ödev
+Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+𝜋 sayısını = 3.14 alınız.
+
+Formül : (𝜋 * (r*r) * 𝛼) / 360
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        float yaricap,cevre,alan,aci,dilimAlan;
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Lutfen bir yaricapi giriniz:");
+        yaricap=yazici.nextFloat();
+        System.out.println("Acinizi giriniz: ");
+        aci=yazici.nextFloat();
+
+        cevre= (float) (2*Math.PI*yaricap);
+        alan= (float)(Math.PI*yaricap*yaricap);
+
+        dilimAlan=(float)((Math.PI*(yaricap*yaricap)*aci)/360);
+        System.out.println("Dairenin Tam Alani: "+alan);
+        System.out.println("Dairenin Tam Cevresi: "+cevre);
+        System.out.println("Dairenin dilimsel alani: "+dilimAlan);
     }
 }
